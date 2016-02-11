d3473ac79d193b08d6b43d919aaf2019a9cfd752
vagrant <vagrant@precise32.(none)>
2016-02-11 06:05:32 +0000

    working slideshow but no end message

---
+++ static/explore.js
@@ -0,0 +1,58 @@
+//var data = require('explore.json');
+
+$(document).ready(function() {
+
+    //get dishes json data 
+    var dishes;
+    //for current dish 
+    var curr;
+    var i = 0; //tracker number
+
+    //get JSON data with first 
+         $.getJSON('explore.json', function(data) {
+        dishes = data;
+        console.log("getJSON activated!");
+        
+        curr = data.dishes[i];
+        console.log(curr);
+        var name = curr.name;
+        console.log("name variable testing..." + name);
+        var im = curr.imgURL;
+        $('#dish_img').attr('src', im);
+        $('item').text(name);
+
+});
+
+         //move to next dish image
+        $('#next').click(function() { 
+            //change current dish number
+                i = i+1;
+                console.log(i);
+                curr = dishes.dishes[i];
+   
+                    console.log(curr);
+                    var im = curr.imgURL;
+                    $('#dish_img').attr('src', im);
+                console.log(dishes.length);
+            
+        }); 
+
+        $('#prev').click(function() { 
+             i = i-1;
+            console.log(i);
+            curr = dishes.dishes[i];
+   
+            console.log(curr);
+            var im = curr.imgURL;
+            $('#dish_img').attr('src', im);
+            console.log(dishes.length);
+        })
+
+})
+ 
+
+
+
+
+
+    
--- static/routes/exp.js
+++ static/routes/exp.js
@@ -2,7 +2,6 @@ var data = require('../explore.json');
 
 exports.view = function(req, res) { 
 	console.log("Explore page testing...")
-	console.log(data);
 	res.render('explore', data);
 
 }
