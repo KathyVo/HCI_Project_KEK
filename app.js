8870712ca65b4ed90ab41e0e7fd0bff1a42e4b83
Kirk Srijongsirikul <srijongsirikul.kirk@gmail.com>
2016-02-11 22:13:25 +0000

    checking to see if heroku works

--- app.js
+++ app.js
@@ -44,5 +44,6 @@ app.get('/bookmarks', bookmarks.view);
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
   console.log('Directory: ' + (path.join(__dirname, 'static')));
-
+  console.log(bookmarks);
+  console.log(index);
 });
